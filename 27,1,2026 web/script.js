fe9353fe191c2f7cd4065985447b32c955a9a8ba
skibidi fortnite enjoyer <jakmih202@gmail.com>
2026-01-27 09:29:53 +0100

    Add files via upload

---
+++ 27,1,2026 web/script.js
@@ -0,0 +1,32 @@
+const chlieb = 1.50;
+const mlieko = 0.90;
+const maslo = 2.30;
+spolu = maslo + mlieko + chlieb
+console.log(`spolu nákup stojí ${maslo + mlieko + chlieb}`)
+
+const kilometer = 5;
+const kmtomile = 0.62137119;
+console.log(`kilometer na míľu je ${kilometer * kmtomile}`)
+
+const gramy = 2500;
+const nakg = 1000;
+console.log(`gramy na kg ${gramy / nakg}`)
+
+const celzia = 20;
+console.log(`celzia na fernheit ${celzia*9/5 + 32}`)
+
+let meno = "Novák";
+let priezvisko = "Peter";
+const lowercasemeno = meno.Tolowercase();
+const uppercasepriezvisko = priezvisko.toUpperCase();
+const celkovyPocetZnakov meno.length + priezvisko.length + 1;
+
+console.log(`Meno a priezvisko: ${lowerCaseMeno} $(upperCasePriezvisko))`;
+console.log(Celkový počet znakov: ${celkovyPocetZnakov));
+
+
+const prvePismenomena meno.charAt(0);
+const prvePismenoPriezviska priezvisko.charAt(0);
+
+console.log(`Prvé písmeno mena: $(prvePismenoMena), Prvé písmeno priezviska: $(prvePismenoPriezviska))`;
+
